1800f0df186bd3abe36096354027b732bbc80fdf
lwh.wcy <lwh.wcy@bytedance.com>
2026-09-19 15:55:01 +0800

    Fix mobile purchase date and price field layout
    Co-authored-by: TRAE CLI <traecli@bytedance.com>

--- web/src/pages/Editor.vue
+++ web/src/pages/Editor.vue
@@ -585,7 +585,7 @@ onUnmounted(() => {
                   >标签<input v-model="tags" placeholder="衬衫，花卉，春夏"
                 /></label>
               </div>
-              <div class="field-row">
+              <div class="field-row purchase-fields">
                 <label
                   >购买日期<input v-model="f.purchaseDate" type="date" /></label
                 ><label
